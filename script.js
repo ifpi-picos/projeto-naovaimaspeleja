092714b688e3c0f740e502d0e1c4eb3980014eaf
Marcos Soares <marcos.paulo.s.m.filho@gmail.com>
2023-07-01 10:22:55 -0300

    starting script

---
+++ script.js
@@ -0,0 +1,8 @@
+const item_list = [];
+
+let input_name = document.getElementById('entry_name');
+let input_value = document.getElementById('entry_value');
+let choose_state = document.getElementsByName('state');
+function insert_item(){
+
+}
