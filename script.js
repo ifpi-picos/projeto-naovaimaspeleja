93fac4db29ec2aa9a3222d3e7b2a89dfb1670e8c
Marcos Soares <marcos.paulo.s.m.filho@gmail.com>
2023-07-02 16:53:27 -0300

    small changes

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// Função para redirecionar para outra página
-function redirectToPage(pageUrl) {
-    window.location.href = pageUrl;
+// Funções para redirecionar para outra página
+function redirectToCadastro() {
+    window.location.href = 'cadastrar_page.html' ;
 }
 
-// Seleciona os botões pelo tipo e adiciona um ouvinte de evento para cada um
-let buttons = document.querySelectorAll('button[type="button"]');
-buttons.forEach(function(button) {
-button.addEventListener('click', function() {
-    // Chama a função de redirecionamento passando a URL da outa página
-    redirectToPage('outra_pagina.html');
-});
-});
+function redirectToLista() {
+    window.location.href = 'lista_page.html' ;
+}
+
+// // Seleciona os botões pelo tipo e adiciona um ouvinte de evento para cada um
+// let buttons = document.querySelectorAll('button[type="button"]');
+// buttons.forEach(function(button) {
+//     button.addEventListener('click', function() {
+//         // Chama a função de redirecionamento passando a URL da outa página
+//         redirectToPage('outra_pagina.html');
+//     });
+// });
